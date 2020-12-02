bce5dc68b5c73b25266c1cdcae9475e34dda06c1
bardo herve <andela@andelas-MacBook-Pro-2.local>
2020-12-02 23:07:22 +0200

    new changes commited

--- src/app.js
+++ src/app.js
@@ -8,10 +8,6 @@ app.get('/', (req, res) => {
   res.json({ status: 'success', message: 'Welcome to my server' });
 });
 
-const PORT = process.env.PORT || 3000;
-
-// eslint-disable-next-line no-console
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
 export default app;
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 import swaggerJsDoc from 'swagger-jsdoc';
 import { serve, setup } from 'swagger-ui-express';
