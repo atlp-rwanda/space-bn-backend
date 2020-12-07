73955148ebad5b9006486aec6c83d7e7e7e066cd
jemmyarina <jemmyarinau@gmail.com>
2020-12-07 18:49:14 +0200

    ft-sequelize-setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
-import express, { json } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.use(json());
+// app.use(json());
+// app.use(urlencoded( { extended:false} ))
 
 app.get('/', (req, res) => {
   res.json({ status: 'success', message: 'Welcome to my server' });
