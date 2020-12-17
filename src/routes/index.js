0927f064327215064e9f2043aa50d7d4eb9edfa9
furebo <furebodidace582@gmail.com>
2020-12-17 15:21:38 +0200

    removes the hotel

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,8 +8,6 @@ const router = Router();
 
 
 const controller = require('../controllers/room');
-const controllerHotel = require('../controllers/hotel');
-const { getHotelRooms } = require('../controllers/hotel');
 
 //Rooms controllers
 
@@ -19,15 +17,6 @@ const getRoom = controller.getRoomById;
 const updateRoom = controller.updateRoom;
 const deleteRoom = controller.deleteRoom;
 
-//Hotel's controllers
-
-const createHotel = controllerHotel.createHotel;
-const deleteHotel = controllerHotel.deleteHotel;
-const getHotels = controllerHotel.getAllHotels;
-const getHotel = controllerHotel.getHotel;
-const updateHotel = controllerHotel.updateHotel;
-//const getHotelRooms = controllerHotel.getHotelRooms
-
 //Rooms routes
 
 router.post('/rooms', createRoom);
@@ -36,14 +25,6 @@ router.get('/rooms/:roomId',getRoom);
 router.put('/rooms/:idroom',updateRoom);
 router.delete('/rooms/:roomId',deleteRoom);
 
-//Hotel's routes
-
-router.post('/hotels',createHotel);
-router.delete('/hotels/:hotelId',deleteHotel);
-router.get('/hotels',getHotels);
-router.get('/hotels/:hotelId',getHotel);
-router.get('/hotels/:hotelId/rooms',getHotelRooms);
-router.put('/hotels/:idhotel',updateHotel);
 
 
 module.exports = router;
--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -10,11 +10,164 @@ const getRoom = controller.getRoomById;
 const deleteRoom = controller.deleteRoom;
 const updateRoom = controller.updateRoom;
 
+/**
+ * @swagger
+ *
+ * /rooms:
+ *    post:
+ *      summary: add a room
+ *      tags: [rooms]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/room'
+ *      responses:
+ *        "201":
+ *          description: Article schema
+ *
+ * components:
+ *    schemas:
+ *      room:
+ *        type: object
+ *        required:
+ *          - roomType
+ *          - description
+ *          - roomLabel
+ *          - hotelId
+ *          - status
+ *          - price 
+ *        properties:
+ *          roomType:
+ *            type: string
+ *          description:
+ *              type: string
+ *          roomLabel:
+ *              type: string
+ *          hotelId:
+ *              type: string
+ *          status:
+ *              type: string
+ *          price:
+ *              type: string
+ *                      
+ *       
+ */
+
 
 router.post('/rooms',createRoom);
+
+/**
+ * @swagger
+ * /rooms/{id}:
+ *   get:
+ *     summary: For getting a single room 
+ *     tags:
+ *       - [Rooms]
+ *     description: Returns a single room
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Particular Room Object's ID (Automatically assigned by database)
+ *         in: path
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: A single room
+ *       500:
+ *         description: Server Error
+ */     
 router.get('/rooms/:id', getRoom);
+
+
+/**
+ * @swagger
+ *
+ * /rooms/{id}:
+ *    put:
+ *      summary: Room update based on ID
+ *      tags: [Room]
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          description: Room ID
+ *          required: true
+ *      requestBody:
+ *        required: false
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/room'
+ *      responses:
+ *        "201":
+ *          description: A Room schema
+ *
+ * components:
+ *    schemas:
+ *      room:
+ *        type: object
+ *        required: 
+ *          - roomType
+ *          - description
+ *          - roomLabel
+ *          - hotelId
+ *          - status
+ *          - price
+ *        properties:
+ *          roomType:
+ *            type: string
+ *          description:
+ *            type: string
+ *          roomLabel:
+ *            type: string
+ *          hotelId: 
+ *            type: integer
+ *          status:
+ *            type: string
+ *          price:
+ *            type: string
+ *            
+ *
+ */
 router.put('/rooms/:idroom',updateRoom);
+
+/**
+ * @swagger
+ * /rooms:
+ *  get:
+ *    tags:
+ *    - All rooms
+ *    summary: All rooms from database
+ *    description: Rooms are desplayed from DB
+ *    responses:
+ *      '200':
+ *        description: Rooms are desplayed succesffuly.
+ *      
+*/
 router.get('/rooms',getRooms);
+
+/**
+ * @swagger
+ * /rooms/{roomid}:
+ *   delete:
+ *     summary: Deletes a room based on ID
+ *     tags:
+ *       - Rooms
+ *     description: Deletes a single Room
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: roomid
+ *         description: room's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
 router.delete('/rooms/:roomid',deleteRoom);
 
 module.exports = router;
