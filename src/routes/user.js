139098d7933a379b396036cc41739035b86bcc16
kabano gilles <gilleskaba@gmail.com>
2020-12-18 19:11:45 +0200

    added the user signup route

---
+++ src/routes/user.js
@@ -0,0 +1,73 @@
+import express from 'express';
+
+const router = express.Router();
+
+const userController = require('../controllers/user');
+const SchemaValidator = require('../middlewares/SchemaValidator');
+
+const validateRequest = SchemaValidator(true);
+
+/**
+ * @swagger
+ * /user/signup:
+ *    post:
+ *      summary: User can signup
+ *      tags: [Users]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/user'
+ *      responses:
+ *        "201":
+ *          description: A user schema
+ *
+ * components:
+ *    schemas:
+ *      user:
+ *        type: object
+ *        required:
+ *          - firstname
+ *          - lastname
+ *          - telephone
+ *          - email
+ *          - password
+ *          - role
+ *          - gender
+ *          - origin
+ *          - profession
+ *          - age
+ *          - identification_type
+ *          - identification_number
+ *        properties:
+ *          firstname:
+ *            type: string
+ *          lastname:
+ *            type: string
+ *          telephone:
+ *            type: string
+ *          email:
+ *            type: string
+ *          password:
+ *            type: string
+ *          role:
+ *            type: string
+ *          gender:
+ *            type: string
+ *          origin:
+ *            type: string
+ *          profession:
+ *            type: string
+ *          age:
+ *            type: integer
+ *          identification_type:
+ *            type: string
+ *          identification_number:
+ *            type: string
+ */
+router.post('/signup', validateRequest, userController.signup);
+
+
+
+module.exports = router;
