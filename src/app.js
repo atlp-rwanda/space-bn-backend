85d9211221aaac7fc1bc07bfd81ff5720c656923
kabano gilles <gilleskaba@gmail.com>
2020-12-18 19:15:11 +0200

    imported the user routes and swagger documentation

--- src/app.js
+++ src/app.js
@@ -1,16 +1,57 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app = express();
 
-// app.use(json());
-// app.use(urlencoded( { extended:false} ))
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+
+const swaggerOptions = {
+	swaggerDefinition: {
+	  openapi: '3.0.0', 
+	  info: {
+		title: 'BareFoot Nomad Project',
+		version: '1.0.0',
+		description: 'Your API description'
+	  },
+	  basePath: '/',
+	  components: {
+		securitySchemes: {
+		  bearerAuth: {
+			type: 'http',
+			scheme: 'bearer',
+			in: 'header',
+			bearerFormat: 'JWT',
+		  }
+		}
+	  },
+	  security: [{
+		bearerAuth: []
+	  }]
+	},
+	apis: ['./src/routes/*.js']
+  };
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use(bodyParser.json());                                     
+app.use(bodyParser.urlencoded({extended: true}));               
+app.use(bodyParser.text());                                    
+app.use(bodyParser.json({ type: 'application/json'})); 
+
+const userRoutes = require('./routes/user');
 
 app.get('/', (req, res) => {
   res.json({ status: 'success', message: 'Welcome to my server' });
 });
+
+app.use('/user', userRoutes);
 
 export default app;
 
