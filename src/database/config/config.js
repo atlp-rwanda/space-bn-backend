7348dface1a067a4c3211c3f2754d4da86b218b5
furebo <furebodidace582@gmail.com>
2020-12-19 12:27:10 +0200

    Implementation for rooms CRUD operations

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -16,4 +16,27 @@ module.exports = {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
   },
+<<<<<<< HEAD
 };
+=======
+}
+
+*/
+//require('dotenv').config()
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const development = {
+  url: process.env.DEV_DATABASE_URL,
+  dialect: 'postgres',
+}
+export const test = {
+  url: process.env.TEST_DATABASE_URL,
+  dialect: 'postgres',
+}
+export const production = {
+  url: process.env.DATABASE_URL,
+  dialect: 'postgres',
+}
+
+>>>>>>> 73cec4b... Implementation for rooms CRUD operations
