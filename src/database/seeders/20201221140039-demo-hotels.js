c26769ce37c140ac93b649f7ae7f6bed6d525de2
leny62 <lihirwe6@gmail.com>
2020-12-21 17:06:22 +0200

    no-edit

--- src/database/seeders/20201221140039-demo-hotels.js
+++ src/database/seeders/20201221140039-demo-hotels.js
@@ -1,24 +1,23 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('hotels', [{
+      hotelname: 'Gorillas Hotel',
+      pricerange: '$120',
+      location: 'Nyamagabe',
+      ranking: '3 star',
+      parking: 'Yes',
+      wifi: 'Yes',
+      swimmingpool: 'no',
+      breakfast: 'Yes',
+      rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
+      images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
+      hotelemail: 'five@yahoo.com',
+      updatedAt: '2020-12-21T13:53:26.104Z',
+      createdAt: '2020-12-21T13:53:26.104Z'
+    }], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('hotels', null, {});
   }
 };
