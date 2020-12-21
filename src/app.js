da2fc4fbd609542d5c8eddb978dd85cd7a897402
leny62 <lihirwe6@gmail.com>
2020-12-21 17:45:58 +0200

    Removing commented lines

--- src/app.js
+++ src/app.js
@@ -4,11 +4,6 @@ import dotenv from 'dotenv';
 
 import hotelRoutes from './routes/hotelRoute';
 
-// const createHotel = require('./routes/hotelRoute');
-// const getHotels = require('./routes/hotelRoute');
-// const deleteHotel = require('./routes/hotelRoute');
-// const getHotel = require('./routes/hotelRoute');
-
 dotenv.config();
 
 const app = express();
@@ -16,18 +11,10 @@ const app = express();
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
-// app.use(json());
-// app.use(urlencoded( { extended:false} ))
-
 app.get('/', (req, res) => {
   res.json({ status: 'success', message: 'Welcome to my server' });
 });
 
 app.use(`${process.env.API_VERSION}/hotels`, hotelRoutes);
-
-// app.use('/createHotel', createHotel);
-// app.use('/allHotels', getHotels);
-// app.use('/deleteHotel/:id', deleteHotel);
-// app.use('/hotel/:id', getHotel);
 
 export default app;
