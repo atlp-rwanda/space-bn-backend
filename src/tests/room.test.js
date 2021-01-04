15ad5f0d31e8bd21958b0bebe661857ea2d6c4b2
furebo <furebodidace582@gmail.com>
2021-01-04 07:47:59 +0200

    Authentication and writes tests for protected endpoints

--- src/tests/room.test.js
+++ src/tests/room.test.js
@@ -23,7 +23,6 @@ describe('Server!', () => {
       });
   });
 });
-<<<<<<< HEAD
 */
 import {chai} from 'chai';
 import { use, request, expect } from 'chai';
@@ -41,50 +40,6 @@ use(chaiHttp);
 describe('Sample Test', () => {
   it('should test that true === true', () => {
     expect(true).to.be.true;
-=======
-describe("Get All Rooms", () => {
-     it("should return an array of the all Rooms", (done) => {
-      chai
-        .request(app)
-        .get("/rooms")
-        .end((err, res) => {
-          if (err) done(err);
-          expect(res).to.have.status(200);    
-          done();
-        });
-    });
-  });
-describe("Get Specific Room", () => {
-  const idroom = 1;
-  it("should return selected room", (done) => {
-    chai
-      .request(app)
-      .get(`/rooms/${idroom}`)
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        done();
-      });
-  });
-});
-  describe("Room Endpoints", () => {
-      const idroom = 1;
-    it("should update a Room", (done) => {
-      chai
-        .request(app)
-        .put(`/rooms/${idroom}`)
-        .send({
-          hotelId: "001",
-          description: "Room for VIP",
-          roomType: "first class",
-          roomLabel: "label 001",
-          status: "double",
-        })
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          done();
-        });
-    });
->>>>>>> 8a244a6... Implementation for rooms CRUD operations
   });
 });
 
