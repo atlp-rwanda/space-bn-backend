2a1bb858717c94acd4e58fa6ca35a46c7e8726d7
furebo <furebodidace582@gmail.com>
2021-01-07 14:32:54 +0200

    chenges the name of the model

--- src/database/models/roommodel.js
+++ src/database/models/roommodel.js
@@ -1,19 +1,21 @@
+
 module.exports = (sequelize, DataTypes) => {
-  const roommodel = sequelize.define('roommodel',{
+  const RoomModel = sequelize.define('RoomModel',{
    roomType: DataTypes.STRING,
    description: DataTypes.STRING,
    roomLabel: DataTypes.STRING,
    hotelId: DataTypes.INTEGER,
    status: DataTypes.STRING,
-   price: DataTypes.STRING
+   price: DataTypes.STRING,
+   roomImage: DataTypes.STRING
   },
   {
    sequelize,
-   modelName: 'roommodel',
+   modelName: 'RoomModel',
    onUpdate: 'CASCADE',
    onDelete: 'CASCADE'
  });
 
   
-   return roommodel;
+   return RoomModel;
  };
