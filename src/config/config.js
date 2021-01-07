93cc40479b7ba2483bddb43a17dce94326dc1923
furebo <furebodidace582@gmail.com>
2021-01-07 12:34:47 +0200

    removes comments in configuration files and run tests

--- src/config/config.js
+++ src/config/config.js
@@ -7,9 +7,6 @@ module.exports = {
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
-    // database: process.env.TEST_DATABASE_URL,
-    // password: process.env.TEST_DB_PASSWORD,
-    // username: process.env.TEST_DB_USERNAME,
     dialect: 'postgres',
   },
   production: {
