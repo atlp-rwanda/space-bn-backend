d73af43cdae79b43e11407d64f3f31e42c566dc9
furebo <furebodidace582@gmail.com>
2021-01-08 17:34:38 +0200

    Getting rooms for a particular hotel and swagger for its endpoint

--- src/controllers/room.js
+++ src/controllers/room.js
@@ -82,7 +82,25 @@ const deleteRoom = async (req, res) => {
   }
 };
 
+const roomByHotel = async (req, res) => {
+  try {
+    const {hotelId} = req.params;
+    const rooms = await model.RoomModel.findAll({
+      where: { hotelId: hotelId }
+      
+    });
+    if (rooms) {
+      return res.status(200).json({ rooms });
+    }
+    
+    return res.status(404).send('Hotel Not found');
+  } catch (error) {
+
+    return res.status(500).send(error.message);
+  }
+};
+
 //Exporting functions
 
-module.exports = {createRoom,getAllRooms,getRoomById,updateRoom,deleteRoom} 
+module.exports = {createRoom,getAllRooms,getRoomById,updateRoom,deleteRoom,roomByHotel} 
 
