34830200addc19f8984e6e768d56c384485114ec
leny62 <lihirwe6@gmail.com>
2021-01-10 22:47:12 +0200

    Adding codes to hotel-tests

--- src/tests/hotelModels.test.js
+++ src/tests/hotelModels.test.js
@@ -4,6 +4,8 @@ import http from 'chai-http';
 
 import app from '../app';
 
+import { token } from './fixtures/defaultData';
+
 process.env.NODE_ENV = 'test';
 
 let hotelId = 1;
@@ -13,96 +15,95 @@ const { expect } = chai;
 chai.use(http);
 
 describe('Testing hotel endpoints', () => {
-  describe('Hotel models', () => {
-    describe('All hotels', () => {
-      it('should return all hotels', (done) => {
-        chai
-          .request(app)
-          .get('/api/v1/hotels/allHotels')
-          .end((err, res) => {
-            if (err) done(err);
-            expect(res).to.have.status(200);
-            done();
-          });
+  it('should return all hotels', (done) => {
+    chai
+      .request(app)
+      .get('/api/v1/hotels/allHotels')
+      .set('Authorization', token)
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res).to.have.status(200);
+        done();
       });
-    });
+  });
+  it('should return the created hotel ', (done) => {
+    chai
+      .request(app)
+      .post('/api/v1/hotels/createHotel')
+      .set('Authorization', token)
+      .send({
+        hotelname: 'Umubavu Hotel',
+        pricerange: '$80',
+        location: 'Musanze',
+        ranking: '3 star',
+        parking: 'Yes',
+        wifi: 'Yes',
+        swimmingpool: 'no',
+        breakfast: 'Yes',
+        rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
+        images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
+        hotelemail: 'five@yahoo.com'
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(201);
+        console.log('===========$$$$$$$', res.body.hotel.hotelId);
+        hotelId = res.body.hotel.hotelId;
+        done();
+      });
+  });
+});
 
-    describe('When the Admin try to create a hotel --/createHotel', () => {
-      it('should return the created hotel ', (done) => {
-        chai
-          .request(app)
-          .post('/api/v1/hotels/createHotel')
-          .send({
-            hotelname: 'Umubavu Hotel',
-            pricerange: '$80',
-            location: 'Musanze',
-            ranking: '3 star',
-            parking: 'Yes',
-            wifi: 'Yes',
-            swimmingpool: 'no',
-            breakfast: 'Yes',
-            rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
-            images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
-            hotelemail: 'five@yahoo.com'
-          })
-          .end((err, res) => {
-            expect(res.status).to.equal(201);
-            console.log('===========$$$$$$$', res.body.hotel.hotelId);
-            hotelId = res.body.hotel.hotelId;
-            done();
-          });
-      });
+describe(' Returning selected hotel', () => {
+// const id = hotelId;
+it('should return selected hotel', (done) => {
+  chai
+    .request(app)
+    .get(`/api/v1/hotels/hotel/${hotelId}`)
+    .set('Authorization', token)
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      done();
     });
+  }); 
+});
 
-    describe('Get Specific hotel', () => {
-      // const id = hotelId;
-      it('should return selected hotel', (done) => {
-        chai
-          .request(app)
-          .get(`/api/v1/hotels/hotel/${hotelId}`)
-          .end((err, res) => {
-            expect(res.status).to.equal(200);
-            done();
-          });
-      });
+describe(' Update selected hotel', () => {
+// const id = hotelId;
+it('should update a hotel', (done) => {
+  chai
+    .request(app)
+    .patch(`/api/v1/hotels/updateHotel/${hotelId}`)
+    .set('Authorization', token)
+    .send({
+      hotelname: 'Amahuwezi Hotel',
+      pricerange: '$80',
+      location: 'Musanze',
+      ranking: '3 star',
+      parking: 'Yes',
+      wifi: 'Yes',
+      swimmingpool: 'no',
+      breakfast: 'depends',
+      rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
+      images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
+      hotelemail: 'five@yahoo.com'
+    })
+    .end((err, res) => {
+      expect(res.status).to.equal(200);
+      done();
     });
+});  
+});
 
-    describe('Update specific hotel', () => {
-      // const id = hotelId;
-      it('should update a hotel', (done) => {
-        chai
-          .request(app)
-          .patch(`/api/v1/hotels/updateHotel/${hotelId}`)
-          .send({
-            hotelname: 'Amahuwezi Hotel',
-            pricerange: '$80',
-            location: 'Musanze',
-            ranking: '3 star',
-            parking: 'Yes',
-            wifi: 'Yes',
-            swimmingpool: 'no',
-            breakfast: 'depends',
-            rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
-            images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
-            hotelemail: 'five@yahoo.com'
-          })
-          .end((err, res) => {
-            expect(res.status).to.equal(200);
-            done();
-          });
+describe(' Delete endpoint selected hotel', () => {
+  // const id = hotelId;
+  it('should delete selected hotel', (done) => {
+    chai
+      .request(app)
+      .delete(`/api/v1/hotels/deleteHotel/${hotelId}`)
+      .set('Authorization', token)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
       });
-    });
-    describe(' Delete endpoint selected hotel', () => {
-      // const id = hotelId;
-      it('should delete selected hotel', (done) => {
-        chai
-          .request(app)
-          .delete(`/api/v1/hotels/deleteHotel/${hotelId}`)
-          .end((err, res) => {
-            expect(res.status).to.equal(200);
-            done();
-          });
-      });
-    });
   });
 });
