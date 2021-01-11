ec247dacb8758df161eea254d4072cacb78d0648
leny62 <lihirwe6@gmail.com>
2021-01-11 19:16:37 +0200

    no-edit

--- src/tests/hotelModels.test.js
+++ src/tests/hotelModels.test.js
@@ -10,7 +10,7 @@ process.env.NODE_ENV = 'test';
 
 let hotelId = 1;
 
-const token = '';
+let token = '';
 
 const { expect } = chai;
 
@@ -23,104 +23,104 @@ describe('Testing hotel endpoints', () => {
       password: '1234567$#8',
     };
     chai.request(app)
-    .post('/user/signin')
-    .send(logged_user)
-    .then((login_response) => {
-      token = 'Bearer '+ login_response.body.token.split[1];
-    });
+      .post('/user/signin')
+      .send(logged_user)
+      .then((login_response) => {
+        token = `JWT ${login_response.body.token.split[1]}`;
+      });
     done();
   });
   describe('Returning all hotels', () => {
-  it('should return all hotels', (done) => {
-    chai
-      .request(app)
-      .get('/api/v1/hotels/allHotels')
-      .set('authorization', 'Bearer ' + token)
-      .end((err, res) => {
-        if (err) done(err);
-        expect(res).to.have.status(200);
-        done();
-      });
+    it('should return all hotels', (done) => {
+      chai
+        .request(app)
+        .get('/api/v1/hotels/allHotels')
+        .set('authorization', token)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it('should return the created hotel ', (done) => {
+      chai
+        .request(app)
+        .post('/api/v1/hotels/createHotel')
+        .set('Authorization', token)
+        .send({
+          hotelname: 'Umubavu Hotel',
+          pricerange: '$80',
+          location: 'Musanze',
+          ranking: '3 star',
+          parking: 'Yes',
+          wifi: 'Yes',
+          swimmingpool: 'no',
+          breakfast: 'Yes',
+          rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
+          images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
+          hotelemail: 'five@yahoo.com'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          console.log('===========$$$$$$$', res.body.hotel.hotelId);
+          hotelId = res.body.hotel.hotelId;
+          done();
+        });
+    });
   });
-  it('should return the created hotel ', (done) => {
-    chai
-      .request(app)
-      .post('/api/v1/hotels/createHotel')
-      .set('Authorization', token)
-      .send({
-        hotelname: 'Umubavu Hotel',
-        pricerange: '$80',
-        location: 'Musanze',
-        ranking: '3 star',
-        parking: 'Yes',
-        wifi: 'Yes',
-        swimmingpool: 'no',
-        breakfast: 'Yes',
-        rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
-        images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
-        hotelemail: 'five@yahoo.com'
-      })
-      .end((err, res) => {
-        expect(res.status).to.equal(201);
-        console.log('===========$$$$$$$', res.body.hotel.hotelId);
-        hotelId = res.body.hotel.hotelId;
-        done();
-      });
-  });
-});
 
-describe(' Returning selected hotel', () => {
-// const id = hotelId;
-  it('should return selected hotel', (done) => {
-    chai
-      .request(app)
-      .get(`/api/v1/hotels/hotel/${hotelId}`)
-      .set('Authorization', token)
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        done();
-      });
+  describe(' Returning selected hotel', () => {
+    // const id = hotelId;
+    it('should return selected hotel', (done) => {
+      chai
+        .request(app)
+        .get(`/api/v1/hotels/hotel/${hotelId}`)
+        .set('Authorization', token)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        });
+    });
   });
-});
 
-describe(' Update selected hotel', () => {
-// const id = hotelId;
-  it('should update a hotel', (done) => {
-    chai
-      .request(app)
-      .patch(`/api/v1/hotels/updateHotel/${hotelId}`)
-      .set('Authorization', token)
-      .send({
-        hotelname: 'Amahuwezi Hotel',
-        pricerange: '$80',
-        location: 'Musanze',
-        ranking: '3 star',
-        parking: 'Yes',
-        wifi: 'Yes',
-        swimmingpool: 'no',
-        breakfast: 'depends',
-        rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
-        images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
-        hotelemail: 'five@yahoo.com'
-      })
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        done();
-      });
+  describe(' Update selected hotel', () => {
+    // const id = hotelId;
+    it('should update a hotel', (done) => {
+      chai
+        .request(app)
+        .patch(`/api/v1/hotels/updateHotel/${hotelId}`)
+        .set('Authorization', token)
+        .send({
+          hotelname: 'Amahuwezi Hotel',
+          pricerange: '$80',
+          location: 'Musanze',
+          ranking: '3 star',
+          parking: 'Yes',
+          wifi: 'Yes',
+          swimmingpool: 'no',
+          breakfast: 'depends',
+          rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
+          images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
+          hotelemail: 'five@yahoo.com'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        });
+    });
   });
-});
 
-describe(' Delete endpoint selected hotel', () => {
+  describe(' Delete endpoint selected hotel', () => {
   // const id = hotelId;
-  it('should delete selected hotel', (done) => {
-    chai
-      .request(app)
-      .delete(`/api/v1/hotels/deleteHotel/${hotelId}`)
-      .set('Authorization', token)
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        done();
-      });
+    it('should delete selected hotel', (done) => {
+      chai
+        .request(app)
+        .delete(`/api/v1/hotels/deleteHotel/${hotelId}`)
+        .set('Authorization', token)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        });
+    });
   });
-});
 });
