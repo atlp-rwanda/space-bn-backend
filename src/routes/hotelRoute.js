ed5918546073f0b419447f5e5d01e53fe43363a1
leny62 <lihirwe6@gmail.com>
2021-01-11 16:44:53 +0200

    adding before in tests

--- src/routes/hotelRoute.js
+++ src/routes/hotelRoute.js
@@ -7,6 +7,41 @@ import protection from '../middlewares/check-auth';
 const router = Router();
 
 router.get('/', (req, res) => res.send('Welcome to Barefoot Nomad'));
+
+/**
+ * @swagger
+ * /Request:
+ *    post:
+ *      tags: [CREATING HOTELS]
+ *      summary: Authenticated user can create a hotel
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/requests'
+ *      responses:
+ *        "200":
+ *          description: A post schema
+ *
+ * components:
+ *    schemas:
+ *      requests:
+ *        type: object
+ *        required:
+ *          - dateStart
+ *          - dateEnd
+ *          - idRoom
+ *
+ *        properties:
+ *          dateStart:
+ *            type: string
+ *          dateEnd:
+ *            type: string
+ *          idRoom:
+ *             type: number
+ */
+
 router.post('/createHotel', protection, createHotel);
 router.get('/allHotels', protection, getAllHotels);
 router.delete('/deleteHotel/:id', protection, deleteHotel);
--- src/tests/hotelModels.test.js
+++ src/tests/hotelModels.test.js
@@ -4,22 +4,38 @@ import http from 'chai-http';
 
 import app from '../app';
 
-import { token } from './fixtures/defaultData';
+// import { token } from './fixtures/defaultData';
 
 process.env.NODE_ENV = 'test';
 
 let hotelId = 1;
+
+const token = '';
 
 const { expect } = chai;
 
 chai.use(http);
 
 describe('Testing hotel endpoints', () => {
+  before((done) => {
+    const logged_user = {
+      email: 'gilleskaba@gmail.com',
+      password: '1234567$#8',
+    };
+    chai.request(app)
+    .post('/user/signin')
+    .send(logged_user)
+    .then((login_response) => {
+      token = 'Bearer '+ login_response.body.token.split[1];
+    });
+    done();
+  });
+  describe('Returning all hotels', () => {
   it('should return all hotels', (done) => {
     chai
       .request(app)
       .get('/api/v1/hotels/allHotels')
-      .set('Authorization', token)
+      .set('authorization', 'Bearer ' + token)
       .end((err, res) => {
         if (err) done(err);
         expect(res).to.have.status(200);
@@ -55,43 +71,43 @@ describe('Testing hotel endpoints', () => {
 
 describe(' Returning selected hotel', () => {
 // const id = hotelId;
-it('should return selected hotel', (done) => {
-  chai
-    .request(app)
-    .get(`/api/v1/hotels/hotel/${hotelId}`)
-    .set('Authorization', token)
-    .end((err, res) => {
-      expect(res.status).to.equal(200);
-      done();
-    });
-  }); 
+  it('should return selected hotel', (done) => {
+    chai
+      .request(app)
+      .get(`/api/v1/hotels/hotel/${hotelId}`)
+      .set('Authorization', token)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
 });
 
 describe(' Update selected hotel', () => {
 // const id = hotelId;
-it('should update a hotel', (done) => {
-  chai
-    .request(app)
-    .patch(`/api/v1/hotels/updateHotel/${hotelId}`)
-    .set('Authorization', token)
-    .send({
-      hotelname: 'Amahuwezi Hotel',
-      pricerange: '$80',
-      location: 'Musanze',
-      ranking: '3 star',
-      parking: 'Yes',
-      wifi: 'Yes',
-      swimmingpool: 'no',
-      breakfast: 'depends',
-      rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
-      images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
-      hotelemail: 'five@yahoo.com'
-    })
-    .end((err, res) => {
-      expect(res.status).to.equal(200);
-      done();
-    });
-});  
+  it('should update a hotel', (done) => {
+    chai
+      .request(app)
+      .patch(`/api/v1/hotels/updateHotel/${hotelId}`)
+      .set('Authorization', token)
+      .send({
+        hotelname: 'Amahuwezi Hotel',
+        pricerange: '$80',
+        location: 'Musanze',
+        ranking: '3 star',
+        parking: 'Yes',
+        wifi: 'Yes',
+        swimmingpool: 'no',
+        breakfast: 'depends',
+        rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
+        images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
+        hotelemail: 'five@yahoo.com'
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
 });
 
 describe(' Delete endpoint selected hotel', () => {
@@ -106,4 +122,5 @@ describe(' Delete endpoint selected hotel', () => {
         done();
       });
   });
+});
 });
