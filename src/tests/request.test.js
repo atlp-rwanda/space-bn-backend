00c7e7638336924fc841ab941d131a2286f52a1b
jemmyarina <jemmyarinau@gmail.com>
2021-01-13 19:26:24 +0200

    swagger-documentation

--- src/tests/request.test.js
+++ src/tests/request.test.js
@@ -9,39 +9,6 @@ import app from '../app';
 
 use(chaiHttp);
 
-
-//POSTING REQUEST
-describe('POSTING AN ACCOMMODATION REQUEST TEST', () => {
-
-  it('it should  POST a request', (done) => {
-
-      const token = " ";
-
-      const valid_input = {
-          email: "jemima1@gmail.com",
-          password: "1234567$#8"
-      }
-        chai.request(app)
-        .post('/user/signin')
-        .send(valid_input)
-        .then((login_response)=>{
-            token = 'Bearer ' + login_response.body.token;
-            chai.request(app)
-            .post('/')
-            .set('authorization', token)
-            .send({
-              dateStart:"01-13-2020",
-              dateEnd: "12-10-2021" ,
-              idRoom:3, 
-            })
-            .end((err, res) => {
-              expect(res.status).to.equal(200);
-              done();
-            })
-        })
-  });
-});
-
 //GETTING REQUEST
 describe('GETTING ALL ACCOMMODATION REQUESTS', () => {
    
