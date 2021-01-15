a271c8d29f2b4fae010b5f9fc04b941c8d7386d7
Leny Pascal IHIRWE <68828382+leny62@users.noreply.github.com>
2021-01-15 17:14:51 +0200

    Removing unnecessary console log

--- src/middlewares/check-auth.js
+++ src/middlewares/check-auth.js
@@ -8,8 +8,6 @@ module.exports = (req, res, next) => {
         req.userData = decoded;
         next();
     } catch (error) {
-        console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', error);
-
         return res.status(401).json({
             message: 'Auth failed'
         });
