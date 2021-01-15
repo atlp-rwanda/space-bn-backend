7a6b9282983c6a1a601b6203198099d0e2822f55
leny62 <lihirwe6@gmail.com>
2021-01-15 02:02:06 +0200

    bringing back models/users

---
+++ src/database/models/user.js
@@ -0,0 +1,45 @@
+import bcrypt from 'bcrypt';
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstname: DataTypes.STRING,
+    lastname: DataTypes.STRING,
+    telephone: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING
+    },
+    password: {
+      type: DataTypes.STRING
+    },
+    roleId: DataTypes.INTEGER,
+    gender: DataTypes.STRING,
+    origin: DataTypes.STRING,
+    profession: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    identification_type: DataTypes.STRING,
+    identification_number: DataTypes.STRING,
+    user_image: DataTypes.STRING,
+    isVerified: DataTypes.BOOLEAN,
+  }, {});
+
+  User.beforeSave((user, options) => {
+    if (user.changed('password')) {
+      user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    }
+  });
+
+  User.prototype.comparePassword = function (passw, cb) {
+    bcrypt.compare(passw, this.password, (err, isMatch) => {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, isMatch);
+    });
+  };
+
+  User.associate = function (models) {
+    // associations can be defined here
+  };
+
+  return User;
+};
