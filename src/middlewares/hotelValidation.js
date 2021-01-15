30f6945c4ae06cd5f788ebee53dd45b20224645c
Leny Pascal IHIRWE <68828382+leny62@users.noreply.github.com>
2021-01-15 18:38:09 +0200

    Update validation

--- src/middlewares/hotelValidation.js
+++ src/middlewares/hotelValidation.js
@@ -6,9 +6,30 @@ export const createHotelValidation = (req, res, next) => {
     hotelname: Joi.string().required(),
     pricerange: Joi.string().required(),
     location: Joi.string().required(),
-    rooms: Joi.string().required(),
-    images: Joi.string().required(),
+    rooms: Joi.array().required(),
+    images: Joi.array().required(),
     hotelemail: Joi.string().email().required()
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).json({ message: error.details[0].message });
+
+  return next();
+};
+
+export const updateHotelValidation = (req, res, next) => {
+  const schema = Joi.object().keys({
+    hotelname: Joi.string().min(5),
+    pricerange: Joi.string().min(1),
+    location: Joi.string().min(2),
+    rooms: Joi.array().min(1),
+    images: Joi.array().min(1),
+    hotelemail: Joi.string().email(),
+    ranking: Joi.string().min(1),
+    parking: Joi.string().min(2),
+    wifi: Joi.string().min(2),
+    swimmingpool: Joi.string().min(2),
+    breakfast: Joi.string().min(2),
   });
 
   const { error } = schema.validate(req.body);
