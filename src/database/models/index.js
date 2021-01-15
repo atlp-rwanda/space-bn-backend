f6561344d446b77bb40a7174a85e64bd59baf962
leny62 <lihirwe6@gmail.com>
2021-01-15 02:12:22 +0200

    editing hotel models

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,35 +1,52 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const envConfigs = require('../config/config');
+import 'dotenv/config';
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import Config from './../../config/config';
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = envConfigs[env];
+const config = Config[env];
 const db = {};
 
+// eslint-disable-next-line import/no-mutable-exports
 let sequelize;
+
 if (config.url) {
   sequelize = new Sequelize(config.url, config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config,
+  );
 }
-
-fs
-  .readdirSync(__dirname)
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(__dirname)
+  .filter(
+    (file) =>
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js',
+  )
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
-
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+export { sequelize };
 module.exports = db;
