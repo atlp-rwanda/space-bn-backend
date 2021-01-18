0313a5090e0f7e71a981d5349271a11bba162bc5
Leny Pascal IHIRWE <68828382+leny62@users.noreply.github.com>
2021-01-18 07:20:35 +0200

    Ft hotel models 175930848 (#28)
    * cloning locally
    
    * [finishes #175743307] configuring unit tests
    
    * [finishes #175743307] configuring unit tests
    
    * Testing the server
    
    * Revert "Ch postgres (#3)"
    
    This reverts commit 48a9c290cc6b83d0bfd97459058c505e61ed3aed.
    
    * fixing bugs associated with Travis-ci
    
    * removing unnecessary sequelize codes
    
    * no-edit
    
    * bg(postgres):fix travis configuration
    
    * bg-travis--sequelize installed as a dependancy
    
    * sequelize configuration and seeding database
    
    * Creating hotel models
    
    * no-edit
    
    * ft(hotel models): making all endpoints work and adding one working test
    
    * ft(hotel-models) : making image and room fields arrays
    
    * no-edit
    
    * ft(hotel-models) : making all tests run
    
    * Removing commented lines
    
    * Removing commented lines
    
    * ft(hotel-models) : Debugging
    
    * ft(hotel-models) : rebasing
    
    * hotel-models-CRUD-operations
    
    * hotel-models-CRUD
    
    * resetting to a working commit
    
    * adding cors
    
    * Adding codes to hotel-tests
    
    * adding authentification to hotelTests
    
    * route protection
    
    * adding before in tests
    
    * no-edit
    
    * no-edit
    
    * no-edit
    
    * no-edit
    
    * no-edit
    
    * no-edit
    
    * Making tests run
    
    * Editing hotel routes
    
    * adding index file to database
    
    * adding index.js file to database
    
    * index in models
    
    * Adding code to src/database/models/index
    
    * savin image files
    
    * bringing back models/users
    
    * rebasing and fixing bugs for tests
    
    * editing hotel models
    
    * editing config file
    
    * modifying server test file
    
    * no-edit
    
    * fixing Travis
    
    * Removing unnecessary console log
    
    * Deleting unnecessary file.
    
    * Removing console log
    
    * Validating hotel models
    
    * Adding Validation
    
    * Adding an update Validation for hotels
    
    * Update validation
    
    * Deleting test_db file which is not used
    
    * deleting unused hotel file from services folder
    
    * Pulling from remote
    
    * bg-fix for test
    
    Co-authored-by: anguandia mike <mikeanguandia@gmail.com>
    Co-authored-by: jemmyarina <jemmyarinau@gmail.com>
    Co-authored-by: furebo <furebodidace582@gmail.com>
    Co-authored-by: kabano <gilles.kaba@gmail.com>

---
+++ src/routes/hotelRoute.js
@@ -0,0 +1,229 @@
+import { Router } from 'express';
+import {
+  getAllHotels, createHotel, deleteHotel, getHotel, getHotelRooms, updateHotel
+} from '../controllers/hotelController';
+import protection from '../middlewares/check-auth';
+import { createHotelValidation, updateHotelValidation } from '../middlewares/hotelValidation';
+
+const router = Router();
+
+/**
+ * @swagger
+ *
+ * /hotels:
+ *    post:
+ *      summary: add a hotel
+ *      tags: [Hotels]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/hotel'
+ *      responses:
+ *        "201":
+ *          description: Article schema
+ *
+ * components:
+ *    schemas:
+ *      hotel:
+ *        type: object
+ *        required:
+ *          - hotelname
+ *          - pricerange
+ *          - location
+ *          - ranking
+ *          - parking
+ *          - wifi
+ *          - swimmingpool
+ *          - breakfast
+ *          - rooms
+ *          - images
+ *          - hotelemail
+ *        properties:
+ *          hotelname:
+ *            type: string
+ *          pricerange:
+ *              type: string
+ *          location:
+ *              type: string
+ *          ranking:
+ *              type: string
+ *          parking:
+ *              type: string
+ *          wifi:
+ *              type: string
+ *          swimmingpool:
+ *              type: string
+ *          breakfast:
+ *              type: string
+ *          rooms:
+ *              type: string
+ *          images:
+ *              type: string
+ *          hotelemail:
+ *              type: string
+ *
+ *
+ *
+ */
+
+router.post('/', protection, createHotelValidation, createHotel);
+
+/**
+ * @swagger
+ * /hotels:
+ *  get:
+ *    tags: [Hotels]
+ *    summary: All hotels from database
+ *    description: Hotels are desplayed from DB
+ *    responses:
+ *      '200':
+ *        description: Hotels are desplayed succesffuly.
+ *
+*/
+
+router.get('/', getAllHotels);
+
+/**
+ * @swagger
+ * /hotels/{id}:
+ *    delete:
+ *      tags: [Hotels]
+ *      summary: Authenticated user can delete a hotel
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/requests'
+ *      responses:
+ *        "200":
+ *          description: A hotel schema
+ *
+ * components:
+ *    schemas:
+ *      hotel:
+ *        type: object
+ *        required:
+ *          - hotelname
+ *          - pricerange
+ *          - location
+ *          - ranking
+ *          - parking
+ *          - wifi
+ *          - swimmingpool
+ *          - breakfast
+ *          - rooms
+ *          - images
+ *          - hotelemail
+ *        properties:
+ *          hotelName:
+ *            type: string
+ *          pricerange:
+ *              type: string
+ *          location:
+ *              type: string
+ *          ranking:
+ *              type: string
+ *          parking:
+ *              type: string
+ *          wifi:
+ *              type: string
+ *          swimmingpool:
+ *              type: string
+ *          breakfast:
+ *              type: string
+ *          rooms:
+ *              type: string
+ *          images:
+ *              type: string
+ *          hotelemail:
+ *              type: string
+ */
+
+router.delete('/:id', protection, deleteHotel);
+
+/**
+ * @swagger
+ * /hotels/{id}:
+ *   get:
+ *     summary: For getting a single hotel
+ *     tags: [Hotels]
+ *
+ *     description: Returns a single hotel
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Particular hotel Object's ID (Automatically assigned by database)
+ *         in: path
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: A single hotel
+ *       500:
+ *         description: Server Error
+ */
+
+router.get('/:id', protection, getHotel);
+
+/**
+ * @swagger
+ * /hotels/{id}:
+ *    patch:
+ *      tags: [Hotels]
+ *      summary: Authenticated user can update a hotel
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/requests'
+ *      responses:
+ *        "200":
+ *          description: A hotel schema
+ *
+  * components:
+ *    schemas:
+ *      hotel:
+ *        type: object
+ *        required:
+ *          - hotelname
+ *          - pricerange
+ *          - location
+ *          - ranking
+ *          - parking
+ *          - wifi
+ *          - swimmingpool
+ *          - breakfast
+ *          - rooms
+ *          - images
+ *          - hotelemail
+ *        properties:
+ *          hotelname:
+ *            type: string
+ *          pricerange:
+ *              type: string
+ *          location:
+ *              type: string
+ *          ranking:
+ *              type: string
+ *          parking:
+ *              type: string
+ *          wifi:
+ *              type: string
+ *          swimmingpool:
+ *              type: string
+ *          breakfast:
+ *              type: string
+ *          rooms:
+ *              type: string
+ *          images:
+ *              type: string
+ *          hotelemail:
+ *              type: string
+ */
+router.patch('/:id', protection, updateHotelValidation, updateHotel);
+
+export default router;
---
+++ src/tests/hotelModels.test.js
@@ -0,0 +1,118 @@
+import chai from 'chai';
+
+import http from 'chai-http';
+import jwt from 'jsonwebtoken';
+
+import app from '../app';
+
+const { expect } = chai;
+
+process.env.NODE_ENV = 'test';
+
+let hotelId = 1;
+
+chai.use(http);
+let token = '';
+
+describe('Testing hotel endpoints', () => {
+  before(() => {
+    const user = {
+      email: 'leny@gmail.com',
+      password: 'furebo@#'
+    };
+    token = `JWT ${jwt.sign(JSON.parse(JSON.stringify(user)), process.env.JWT_KEY, { expiresIn: '1h' })}`;
+    jwt.verify(token, process.env.JWT_KEY, (err, data) => {
+      console.log(err, data);
+    });
+  });
+  describe('Getting hotels', () => {
+    it('should return all hotels', (done) => {
+      chai
+        .request(app)
+        .get('/hotels')
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it('should create hotel ', (done) => {
+      chai.request(app)
+        .post('/hotels')
+        .set('authorization', token)
+        .send({
+          hotelname: 'Umubavu Hotel',
+          pricerange: '$80',
+          location: 'Musanze',
+          ranking: '3 star',
+          parking: 'Yes',
+          wifi: 'Yes',
+          swimmingpool: 'no',
+          breakfast: 'Yes',
+          rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
+          images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
+          hotelemail: 'five@yahoo.com'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          hotelId = res.body.hotel.hotelId;
+          done();
+        });
+    });
+  });
+});
+
+describe(' Returning selected hotel', () => {
+  // const id = hotelId;
+  it('should return selected hotel', (done) => {
+    chai
+      .request(app)
+      .get(`/hotels/${hotelId}`)
+      .set('authorization', token)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+});
+
+describe(' Update selected hotel', () => {
+  // const id = hotelId;
+  it('should update a hotel', (done) => {
+    chai
+      .request(app)
+      .patch(`/hotels/${hotelId}`)
+      .set('authorization', token)
+      .send({
+        hotelname: 'Amahuwezi Hotel',
+        pricerange: '$80',
+        location: 'Musanze',
+        ranking: '3 star',
+        parking: 'Yes',
+        wifi: 'Yes',
+        swimmingpool: 'no',
+        breakfast: 'depends',
+        rooms: ['Double rooms', 'Single rooms', 'complex rooms'],
+        images: ['www.unsplash.com/umubavu', 'www.gettyimages/umubavuhotel'],
+        hotelemail: 'five@yahoo.com'
+      })
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+});
+
+describe(' Delete endpoint selected hotel', () => {
+  // const id = hotelId;
+  it('should delete selected hotel', (done) => {
+    chai
+      .request(app)
+      .delete(`/hotels/${hotelId}`)
+      .set('authorization', token)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+});
