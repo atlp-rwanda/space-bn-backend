a82929e52c0348a74eb4c81d10a42da0421e42f7
kabano gilles <gilleskaba@gmail.com>
2021-01-18 23:16:10 +0200

    improved the test

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,17 +1,12 @@
+/* eslint-disable no-console */
 import chai from 'chai';
-
 import http from 'chai-http';
-
-import app from '../app';
-
-import { User } from '../database/models';
-
 import fs from 'fs';
-
+import app from '../app';
+import generateToken from '../utils/genToken';
 chai.use(http);
-
 const { expect } = chai;
-
+const userId = 1;
 describe('User registration', () => {
   it('should return 201 and confirmation for valid input', (done) => {
     chai.request(app).post('/user/signup')
@@ -29,7 +24,6 @@ describe('User registration', () => {
       .field('identification_number', '1122020333')
       .attach('user_image',
         fs.readFileSync('./src/tests/malume.png'), 'malume.png')
-
       .then((res) => {
         expect(res).to.have.status(201);
         expect(res.body.message).to.be.equal('User registered');
@@ -51,7 +45,6 @@ describe('User registration', () => {
         console.log(err);
       });
   });
-
   // test for invalid input
   it('should return 422 for invalid email input', (done) => {
     chai.request(app).post('/user/signup')
@@ -69,7 +62,6 @@ describe('User registration', () => {
       .field('identification_number', '1122020333')
       .attach('user_image',
         fs.readFileSync('./src/tests/malume.png'), 'malume.png')
-
       .then((res) => {
         expect(res).to.have.status(422);
         done();
@@ -78,7 +70,6 @@ describe('User registration', () => {
         console.log(err);
       });
   });
-
   // test an existing e-mail
   it('Should return error 409 when email already registered', (done) => {
     chai.request(app).post('/user/signup')
@@ -96,7 +87,6 @@ describe('User registration', () => {
       .field('identification_number', '1122020333')
       .attach('user_image',
         fs.readFileSync('./src/tests/malume.png'), 'malume.png')
-
       .then((res) => {
         expect(res).to.have.status(409);
         expect(res.body.message).to.be.equal('Email already registered');
@@ -107,7 +97,6 @@ describe('User registration', () => {
       });
   });
 });
-
 describe('User Signin', () => {
   it('should return error 401 for invalid email', (done) => {
     // mock invalid user input
@@ -127,7 +116,6 @@ describe('User Signin', () => {
         console.log(err.message);
       });
   });
-
   it('should return error 401 for invalid credentials', (done) => {
     // mock invalid user input
     const wrong_input = {
@@ -141,7 +129,6 @@ describe('User Signin', () => {
         // console.log(res.body);
         // assertions
         expect(res).to.have.status(401);
-
         expect(res.body.success).to.be.equal(false);
         expect(res.body.message).to.be.equal('Authentication failed. Wrong password.');
         done();
@@ -150,7 +137,6 @@ describe('User Signin', () => {
         console.log(err.message);
       });
   });
-
   it('should return 200 and token for valid credentials', (done) => {
     // mock invalid user input
     const valid_input = {
@@ -170,126 +156,81 @@ describe('User Signin', () => {
         console.log(err);
       });
   });
-
   it('should return 400 when a bad a request is made', (done) => {
-      const bad_request = {
-       
-      }
-      chai.request(app).post('/user/signin')
-        .send(bad_request)
-          .then((res) => {
-            expect(res).to.have.status(400);
-            done();
-          }).catch(err => {
-            console.log(err);
-          })
+    const bad_request = {
+    };
+    chai.request(app).post('/user/signin')
+      .send(bad_request)
+      .then((res) => {
+        expect(res).to.have.status(400);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+});
+let token = '';
+/** Start of crud operations on user */
+describe('USERS', () => {
+  before(async () => {
+    const user = {
+      email: 'keza@gmail.com',
+      password: 'Kabonoj@#'
+    };
+    token = await generateToken(user);
+  });
+  describe('/GET users', () => {
+    it('should return all users', (done) => {
+      chai.request(app).get('/user').set('Authorization', token)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res).to.have.status(200);
+          done();
+        });
     });
-})
-
-
-describe('/GET users', () => {
-
-	it('should return all users', () => {
-    
-    const user_credentials = {
-      "email": "keza@gmail.com",
-      "password": "1234567avb$#8"
-    }
-    chai.request(app)
-        .post('/user/signin')
-        .send(user_credentials)
-        .then((login_response) => {
-          let token = 'Bearer ' + login_response.body.token;
-          chai.request(app).get('/user/').set('Authorization', token)
-              .end((err, res) => {
-                expect(res).to.have.status(200);
-              })
-          }).catch(err => {
-              console.log(err);
-          })
   });
-})
-
+});
 describe('/GET/:id users', () => {
-
-	it('it should GET a user by the given id', () => {
-    
-    const user_credentials = {
-      "email": "keza@gmail.com",
-      "password": "1234567avb$#8"
-    }
-    let user = new User({firstname: "kalisa", lastname: "wilson", email: "wizo@gmail.com", password: "12345678#$"});
-
+  it('it should GET a user by the given id', (done) => {
     chai.request(app)
-        .post('/user/signin')
-        .send(user_credentials)
-        .then((login_response) => {
-          let token = 'Bearer ' + login_response.body.token;
-          user.save((err, user) => {
-            chai.request(app)
-                .get('/user/' + user.id)
-                .send(user)
-                .set('Authorization', token)
-                .end((err, res) => {
-                  expect(res).to.have.status(200);
-                })
-            })
-          });
+      .get(`/user/${userId}`)
+      .set('Authorization', token)
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res).to.have.status(200);
+        done();
+      });
   });
-
-})
-
+  it('it should GET a user by the given id', (done) => {
+    chai.request(app)
+      .get(`/user/${userId}`)
+      .set('Authorization', token)
+      .end((err, res) => {
+        if (err) done(err);
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
 describe('/PUT/:id users', () => {
-
-	it('it should update a user by the given id', () => {
-    
-    const user_credentials = {
-      "email": "keza@gmail.com",
-      "password": "1234567avb$#8"
-    }
-    let user = new User({firstname: "kalisa", lastname: "wilson", email: "wizo@gmail.com", password: "12345678#$"});
-
+  it('it should update a user by the given id', () => {
     chai.request(app)
-        .post('/user/signin')
-        .send(user_credentials)
-        .then((login_response) => {
-          let token = 'Bearer ' + login_response.body.token;
-          user.save((err, user) => {
-            chai.request(app)
-                .put('/user/' + user.id)
-                .send({firstname: "Mussa", lastname: "wilson"})
-                .set('Authorization', token)
-                .end((err, res) => {
-                  expect(res).to.have.status(200);
-                })
-            })
-          });
+      .put(`/user/${userId}`)
+      .send({ firstname: 'Mussa', lastname: 'wilson' })
+      .set('Authorization', token)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+      });
   });
-})
-
+});
 describe('/DELETE/:id users', () => {
-
-	it('it should delete a user by the given id', () => {
-    
-    const user_credentials = {
-      "email": "keza@gmail.com",
-      "password": "1234567avb$#8"
-    }
-    let user = new User({firstname: "Peter", lastname: "wilson", email: "wizo@gmail.com", password: "12345678#$"});
-
+  it('it should delete a user by the given id', () => {
     chai.request(app)
-        .post('/user/signin')
-        .send(user_credentials)
-        .then((login_response) => {
-          let token = 'Bearer ' + login_response.body.token;
-          user.delete((err, user) => {
-            chai.request(app)
-                .get('/user/' + user.id)
-                .set('Authorization', token)
-                .end((err, res) => {
-                  expect(res).to.have.status(200);
-                })
-            })
-          });
+      .delete(`/user/${userId}`)
+      .set('Authorization', token)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+      });
   });
-})
+});
