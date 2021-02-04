a6570e77c5ee1de9e1d398dba6e0d5708b001236
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-02-04 15:05:05 +0200

    ft(question): Any user can add a question & read FAQ

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,58 +1,55 @@
+import Sequelize from 'sequelize';
+import model from '../database/models';
 
-import model from "../database/models";
-import Sequelize from 'sequelize'
+const { Op } = Sequelize;
 
-const Op = Sequelize.Op;
+const returnQueries = (query) => {
+  const queries = [];
+  for (const key in query) {
+    const value = query[key];
 
-const returnQueries = (query) =>{
-    const queries = [];
-    for (const key in query) {
-      const value = query[key];
-      
-      if (key === 'requestStatus' || key === 'requesterName' || key === 'hotelName' || key === 'location' || key === 'roomType' ) queries.push({ [key]: { [Op.iLike]: `%${value}%` } }) ;
-      else queries.push( { [key]: value })
-      
-    }
-    return queries;
-}
+    if (key === 'requestStatus' || key === 'requesterName' || key === 'hotelName' || key === 'location' || key === 'roomType') queries.push({ [key]: { [Op.iLike]: `%${value}%` } });
+    else queries.push({ [key]: value });
+  }
+  return queries;
+};
 
 export const searchRequest = async (req, res) => {
-  try {  
+  try {
     const queries = returnQueries(req.query);
     const { id } = req.userData;
     const Request = await model.request.findAll({
-      where: { idUser:id, [Op.and]: queries,  },
-      include:[
+      where: { idUser: id, [Op.and]: queries, },
+      include: [
         {
-          model:model.User,
-          as:"Requester",
+          model: model.User,
+          as: 'Requester',
         }
       ]
     });
-   
+
     res.status(200).json({ Request });
   } catch (error) {
-    res.status(400).json({message:error.message});
+    res.status(400).json({ message: error.message });
   }
-  };
+};
 
-  export const managerSearchRequestForRequester = async (req, res) => {
-    try {
-      
-      const { id } = req.userData;
-      const queries = returnQueries(req.query);
-  
-      const Request = await model.request.findAll({
-        where: { [Op.and]: queries,  },
-        include:[
-          {
-            model:model.User,
-            as:"Requester",
-          }
-        ]
-      });
-      res.status(200).json({ Request: Request.filter((request)=> (request.idUser !== id && request.Requester.managerId === id ) )});
-    } catch (error) {
-      res.status(400).json({message:error.message});
-    }
-  };
+export const managerSearchRequestForRequester = async (req, res) => {
+  try {
+    const { id } = req.userData;
+    const queries = returnQueries(req.query);
+
+    const Request = await model.request.findAll({
+      where: { [Op.and]: queries, },
+      include: [
+        {
+          model: model.User,
+          as: 'Requester',
+        }
+      ]
+    });
+    res.status(200).json({ Request: Request.filter((request) => (request.idUser !== id && request.Requester.managerId === id)) });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
